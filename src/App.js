d37bf6d1b3680c4046d277786a3233ccf0572056
Mike Wheaton <mike@mikeBook.local>
2018-07-21 08:21:37 -0700

    Quick update to default content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React, { Component } from 'react';
+
+import logo from './logo.svg';
 
 class App extends Component {
   render() {
@@ -8,10 +10,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React People</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          React People lists and connects React developers around the world.
         </p>
       </div>
     );
