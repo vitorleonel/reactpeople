8d67915423a1b9c8bbda69af5c9a6f070d9b67f7
Vitor Leonel <falecom@vitorleonel.com>
2018-08-11 20:41:29 -0300

    config tailwindcss

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="w-screen h-screen flex items-center justify-center">
+        Welcome to <strong className="pl-1">React People</strong>.
+      </div>
+    );
+  }
+}
+
+export default App;
