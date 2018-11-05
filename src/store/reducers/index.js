d9641e52e2c6e9dba106be98d294895c6d554ed1
Vitor Leonel <falecom@vitorleonel.com>
2018-11-05 10:01:00 +0000

    initialize reducers

---
+++ src/store/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import auth from './auth';
+import users from './users';
+
+export default combineReducers({
+  auth,
+  users,
+});
---
+++ src/store/reducers/users.js
@@ -0,0 +1,8 @@
+const INITIAL_STATE = [];
+
+export default function users(state = INITIAL_STATE, action) {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
