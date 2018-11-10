36bc7761535b2d8a56bc81596ffe86ed9e15f879
Vitor Leonel <falecom@vitorleonel.com>
2018-11-10 05:17:26 +0000

    api service

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import queryString from 'query-string';
+
+import api from '../../services/api';
 
 export default class Auth extends Component {
 
   componentDidMount() {
-    console.log(this.props);
+    const { code } = queryString.parse(this.props.location.search);
+
+    if(code)
+      this.authHandler(code);
+  }
+
+  authHandler = async code => {
+    const response = await api.post('/auth', { code });
+
+    console.log(response);
+
+    this.props.history.push('/');
   }
 
   render() {
