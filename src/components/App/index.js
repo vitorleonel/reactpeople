d9c2b36f1ce781ad6a672b1708a538e4a67710f0
Vitor Leonel <falecom@vitorleonel.com>
2018-11-10 05:01:27 +0000

    init react-router-dom

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { GlobalStyle, Wrapper } from './styles';
 
 import MainMap from '../MainMap';
 import SignIn from '../SignIn';
+
+import AuthPage from '../../pages/auth';
 
 const App = () => (
   <Wrapper>
     <GlobalStyle />
     <MainMap />
     <SignIn />
+
+    <BrowserRouter>
+      <Switch>
+        <Route path="/auth" component={AuthPage} />
+      </Switch>
+    </BrowserRouter>
   </Wrapper>
 );
 
---
+++ src/pages/auth/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Auth extends Component {
+
+  componentDidMount() {
+    console.log(this.props);
+  }
+
+  render() {
+    return <React.Fragment />;
+  }
+
+}
