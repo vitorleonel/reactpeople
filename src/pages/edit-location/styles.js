7f681e608bf69a083e3451ed7a93d6372bd80952
Vitor Leonel <falecom@vitorleonel.com>
2018-11-11 02:01:56 +0000

    change position

--- src/pages/edit-location/styles.js
+++ src/pages/edit-location/styles.js
@@ -2,14 +2,15 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 20%;
-  padding: 1.5rem;
+  height: 4rem;
+  line-height: 4rem;
   background-color: ${props => !props.confirm ? 'white' : props.theme.colorNeutralDark};
   color: ${props => !props.confirm ? props.theme.colorNeutralDark : props.theme.colorNeutralLight};
   box-shadow: ${props => props.theme.shadow};
   border-radius: ${props => props.theme.radius};
   text-align: center;
   position: absolute;
-  bottom: 2rem;
+  top: 1rem;
   left: 50%;
   margin-left: -10%
   z-index: 999;
