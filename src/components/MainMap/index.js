69f4dbb07bdae3020c3376325bb82e205787592e
= <vitorleoneloficial@gmail.com>
2018-11-13 14:02:54 -0200

    show users on map

--- src/components/MainMap/index.js
+++ src/components/MainMap/index.js
@@ -3,7 +3,19 @@ import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Map, TileLayer, ZoomControl, Marker } from 'react-leaflet';
 
+import api from '../../services/api';
+
 class MainMap extends Component {
+  componentDidMount() {
+    this.getUsersHandler();
+  }
+
+  getUsersHandler = async () => {
+    const { data } = await api.get('/users');
+
+    this.props.setUsers(data.users);
+  }
+
   newLocationHandler = e => {
     if(!window.location.pathname.includes('/profile/edit-your-location'))
       return;
@@ -19,19 +31,21 @@ class MainMap extends Component {
 
       <ZoomControl position="bottomright" />
 
-      { this.props.markerLocation
-        ? <Marker position={this.props.markerLocation} />
-        : '' }
+      { this.props.markerLocation ? <Marker position={this.props.markerLocation} /> : '' }
+
+      { this.props.users.map(user => <Marker position={[user.location.coordinates[1], user.location.coordinates[0]]} key={user._id} />) }
     </Map>
   }
 }
 
 const mapStateToProps = state => ({
   markerLocation: state.auth.location,
+  users: state.users,
 });
 
 const mapDispatchToProps = dispatch => ({
   setLocation: location => dispatch({ type: 'SET_LOCATION', payload: location }),
+  setUsers: users => dispatch({ type: 'SET_USERS', payload: users }),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(MainMap));
--- src/store/reducers/users.js
+++ src/store/reducers/users.js
@@ -1,7 +1,9 @@
-const INITIAL_STATE = [];
 
-export default function users(state = INITIAL_STATE, action) {
+export default function users(state = [], action) {
   switch(action.type) {
+    case 'SET_USERS':
+      return [ ...action.payload ];
+
     default:
       return state;
   }
