1009e118d5fc59413cd2e60f9e6a57c17d405b30
Vitor Leonel <falecom@vitorleonel.com>
2018-11-15 08:33:53 +0000

    zoom out on map

--- src/components/MainMap/index.js
+++ src/components/MainMap/index.js
@@ -24,7 +24,7 @@ class MainMap extends Component {
   }
 
   render() {
-    return <Map center={[4.5609245, -33.1948357]} zoom={3.5} zoomControl={false} style={{height: '100vh'}} onClick={this.newLocationHandler}>
+    return <Map center={[7.5609245, -28.1948357]} zoom={3} zoomControl={false} style={{height: '100vh'}} onClick={this.newLocationHandler}>
       <TileLayer
         url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}"
       />
